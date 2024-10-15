9b8a1feacd591afc2ffc10e8b348a5c56987ce9e
mihai <mihai@gmail.com>
2024-10-15 08:56:45 +0300

    Updates

---
+++ precache-manifest.abf59e0a1f641ecb41cfabc93a1ef29c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9bae8f85eb7872f9773500ade979fa",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "e77781b4a1e8f823b745",
+    "url": "/react-gh-pages/static/css/main.c6001462.chunk.css"
+  },
+  {
+    "revision": "86ef8ed4f42c63e0a9b3",
+    "url": "/react-gh-pages/static/js/2.7acece96.chunk.js"
+  },
+  {
+    "revision": "0f3164a6ce11391ae42f81b8000729de",
+    "url": "/react-gh-pages/static/js/2.7acece96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e77781b4a1e8f823b745",
+    "url": "/react-gh-pages/static/js/main.41efde25.chunk.js"
+  },
+  {
+    "revision": "3cb497884aa18125fb5a",
+    "url": "/react-gh-pages/static/js/runtime-main.09be2e59.js"
+  },
+  {
+    "revision": "d51aeabfdbea7842d1837865de94a451",
+    "url": "/react-gh-pages/static/media/app-debug.d51aeabf.apk"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  }
+]);
